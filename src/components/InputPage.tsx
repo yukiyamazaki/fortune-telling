f65b75f875a6e948c1f7c2d8c3288ac2810f30c4
YUKI YAMAZAKI <gooo0523@gmail.com>
2023-03-20 01:14:55 +0900

    fixl

--- src/components/InputPage.tsx
+++ src/components/InputPage.tsx
@@ -10,8 +10,32 @@ const InputPage = () => {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    // const [birthYear, birthMonth, birthDay, birthHour] = e.target;
+    const { name, value } = e.target as { name: string };
+
+    console.log('AAAAA', name, value);
     // Validate and calculate Bazi fortune
     // Redirect to ResultPage with results
+    const luckLevels = ['非常に良い', '良い', '普通', '悪い', '非常に悪い'];
+
+    const adviceList = [
+      '注意して行動する',
+      'チャレンジを試みる',
+      '忍耐が大切',
+      '新しい出会いを楽しむ',
+      '変化に適応する',
+      '自分を信じて進む',
+      '計画を立てて行動する',
+      '周りの意見を聞く',
+      'リラックスする時間を作る',
+      '目標に向かって努力する',
+    ];
+
+    // const totalValue = birthYear + birthMonth + birthDay + birthHour;
+    // const luckIndex = totalValue % luckLevels.length;
+    // const adviceIndex = totalValue % adviceList.length;
+
+    // return { luck: luckLevels[luckIndex], advice: adviceList[adviceIndex] };
   };
 
   return (
